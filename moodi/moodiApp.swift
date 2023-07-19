93a1e63599ed5ad2fdf23760e4ad9da94f4428b3
Leyna <leynaphan16@gmail.com>
2023-07-19 13:12:47 -0500

    Initial Commit

---
+++ moodi/moodiApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodiApp.swift
+//  moodi
+//
+//  Created by Leyna Phan on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct moodiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
